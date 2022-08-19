6ecc0723e55a14d55794cf7851a83cdd9d24a525
krishna98016 <32084960+krishna98016@users.noreply.github.com>
2022-08-19 07:01:19 +0530

    Add files via upload

---
+++ Assignment_1/question_3.py
@@ -0,0 +1,7 @@
+# 3. Write a python script and store it in a file and execute the file from the command line.
+
+name="krishna kumar jaiswal"
+roll_no=12
+due_fee=5000.25
+
+print(name,roll_no,due_fee,sep=",")
---
+++ Assignment_1/question_9.py
@@ -0,0 +1,7 @@
+# 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+
+name="krishna jaiswal"
+age=25
+qual="Graduate"
+exp=0
+print(name,age,qual,exp)
